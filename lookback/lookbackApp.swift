aaf2fe251c7225e65119db625be5f2394fcafbe7
Gregory Klein <gregory@incipia.co>
2020-11-23 11:38:13 -0500

    Initial Commit

---
+++ lookback/lookbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  lookbackApp.swift
+//  lookback
+//
+//  Created by Gregory Klein on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct lookbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
